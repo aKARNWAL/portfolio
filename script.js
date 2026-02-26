a91a581f391021a761b0eab7014f59e6dcc690f7
Anuj Karnwal <154620095+aKARNWAL@users.noreply.github.com>
2026-02-26 23:51:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple button click interaction
+// document.querySelectorAll(".circle").forEach(button => {
+//     button.addEventListener("click", function() {
+//         alert(this.innerText + " section coming soon!");
+//     });
+// });
